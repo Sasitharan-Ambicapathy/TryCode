2e3d37536af5f89ee1b38a23b67b55cafecd8382
Sasitharan-Ambicapathy <35830996+Sasitharan-Ambicapathy@users.noreply.github.com>
2021-01-06 14:35:32 +0530

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.By;
+
+public class HomePage {
+    public static By closeButton=By.xpath("//button[@class='_2KpZ6l _2doB4z']");
+    public static By myProfile=By.xpath("//a//div[text()='My Profile']");
+    public static By login=By.xpath("//a[text()='Login']");
+    public static By requestOTP=By.xpath("//button[text()='Request OTP']");
+
+}
